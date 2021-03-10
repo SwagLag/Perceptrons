db6ec3c3c66ab3a9e95b1744fe362d3148858c90
SwagLag <50711973+SwagLag@users.noreply.github.com>
2021-03-10 11:01:29 +0100

    More backpropagation examples implemented.

--- tests/NeuronNetworkBackpropagation/test_AND_backpropagation.py
+++ tests/NeuronNetworkBackpropagation/test_AND_backpropagation.py
@@ -11,3 +11,15 @@ ntwrk = NeuronNetwork([l1],1)
 
 ntwrk.train([[0,0],[0,1],[1,0],[1,1]],[[0],[0],[0],[1]],1000,0.0000001)
 print(ntwrk.__str__())
+
+print("Should be as close as possible to high (1)")
+print("[1,1] gives:")
+print(ntwrk.feed_forward([1,1]))
+print("Should be as close as possible to low (0)")
+print("[0,1] gives:")
+print(ntwrk.feed_forward([0,1]))
+print("[1,0] gives:")
+print(ntwrk.feed_forward([1,0]))
+print("[0,0] gives:")
+print(ntwrk.feed_forward([0,0]))
+
